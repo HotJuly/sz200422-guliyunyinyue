886788ea45e4776f8094452d5349fa51ec689450
HotJuly <949588380@qq.com>
2020-09-22 14:31:02 +0800

    4+5.首页轮播图模块-数据请求+列表渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      bannerList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    /*
+      1.在哪发
+       onLoad
+      2.怎么发
+       wx.request({url})
+      3.往哪发
+        去看接口文档
+    */
+    wx.request({
+      url:"http://localhost:3000/banner",
+      data:{
+        type:2
+      },
+      success:(res)=>{
+        //请求成功之后,将数据存放到状态data中,实现动态渲染
+        console.log(res.data.banners)
+        this.setData({
+          bannerList:res.data.banners
+        })
+      }
+    })
   },
 
   /**

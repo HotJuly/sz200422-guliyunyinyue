4cf1a0b934e3524191139af81741bca14297086a
HotJuly <949588380@qq.com>
2020-09-24 11:32:47 +0800

    22.视频模块(常见功能)

--- pages/video/video.js
+++ pages/video/video.js
@@ -8,7 +8,8 @@ Page({
   data: {
     navList:[],
     currentId: null,
-    videoList:[]
+    videoList:[],
+    trigger:false
   },
   async changeId(event){
     // console.log('changeId')
@@ -20,7 +21,7 @@ Page({
 
     //同步设置data中的currentId
     this.setData({
-      currentId: event.currentTarget.id*1,
+      currentId: event.currentTarget.dataset.id,
       videoList:[]
     })
 
@@ -43,6 +44,145 @@ Page({
       videoList: videoListData.datas
     })
     // console.log(1)
+  },
+  async refresherRefresh(){
+    /*下拉刷新操作
+      1.开启下拉动画->refresher-enabled
+      2.监听用户下拉动作->事件名refresherrefresh
+      3.在事件回调函数内部,发送请求,获取当前最新数据
+      4.将数据更新到data中
+
+      问题:数据请求回来了,但是下拉动画不消失
+      解决:通过状态,控制标签属性refresher-triggered属性的切换
+    */
+    // console.log('refresherRefresh')
+    await this.getVideoListData();
+    this.setData({
+      trigger:false
+    })
+  },
+  scrollToLower(){
+    console.log('scrollToLower')
+    //请求接下来的数据,并与旧数据进行合并
+    let data;
+    setTimeout(()=>{
+      data = [
+          {
+            "type": 1,
+            "displayed": false,
+            "alg": "onlineHotGroup",
+            "extAlg": null,
+            "data": {
+              "alg": "onlineHotGroup",
+              "scm": "1.music-video-timeline.video_timeline.video.181017.-295043608",
+              "threadId": "R_VI_62_57B282070780439AB8B7EEBADA7B0BF4",
+              "coverUrl": "https://p1.music.126.net/9rQe8QvlonyK_6gjUs-KBw==/109951163405417082.jpg",
+              "height": 720,
+              "width": 1280,
+              "title": "Wake (Live ) - Hillsong Young & Free",
+              "description": "该回来的，总会回来",
+              "commentCount": 116,
+              "shareCount": 356,
+              "resolutions": [
+                {
+                  "resolution": 240,
+                  "size": 34912508
+                },
+                {
+                  "resolution": 480,
+                  "size": 58638761
+                },
+                {
+                  "resolution": 720,
+                  "size": 81994785
+                }
+              ],
+              "creator": {
+                "defaultAvatar": false,
+                "province": 1000000,
+                "authStatus": 0,
+                "followed": false,
+                "avatarUrl": "http://p1.music.126.net/yX7jUxKx0MQsKKHm09TdKQ==/109951162889224877.jpg",
+                "accountStatus": 0,
+                "gender": 1,
+                "city": 1002700,
+                "birthday": 809712000000,
+                "userId": 250935934,
+                "userType": 0,
+                "nickname": "DJGrandMother",
+                "signature": "心情如歌",
+                "description": "",
+                "detailDescription": "",
+                "avatarImgId": 109951162889224880,
+                "backgroundImgId": 109951164570679090,
+                "backgroundUrl": "http://p1.music.126.net/lnHCc8D-vLK3E7AphhCO6w==/109951164570679086.jpg",
+                "authority": 0,
+                "mutual": false,
+                "expertTags": null,
+                "experts": null,
+                "djStatus": 0,
+                "vipType": 11,
+                "remarkName": null,
+                "avatarImgIdStr": "109951162889224877",
+                "backgroundImgIdStr": "109951164570679086",
+                "avatarImgId_str": "109951162889224877"
+              },
+              "urlInfo": {
+                "id": "57B282070780439AB8B7EEBADA7B0BF4",
+                "url": "http://vodkgeyttp9.vod.126.net/vodkgeyttp8/P8DZRued_1777476644_shd.mp4?ts=1600919817&rid=67045BE5AADF7E81410E9AF64893300E&rl=3&rs=owHvOhzRHwjbLkDlIdLYmEYcpddxViic&sign=33d87cbee10371f85374a1f2bea82a9f&ext=MbQfiMjsqTjGUrkgAnxbxsSylRxH5VhHzFV5ThhjfyiSCkcYqls2QnMPFzPFNGf2TzxMdlMd3x85Re1upgk5GucHgnVYTYzUr%2BxJl2GJta0KMD2nLWP%2BHVZ3D0Kpgp%2BFprsHzNg%2F0k3A8ZmrhP57eQRGhbFLB%2BDO4NukIn%2BPPUcgxDMeBXvSSet8RB2N86GBR%2F0dG%2FSoju1XlcCQhMHVQtP3pMZpRL9aYLx%2Bg8qHbKJqZUsE8IvZefqWchakvvSO",
+                "size": 81994785,
+                "validityTime": 1200,
+                "needPay": false,
+                "payInfo": null,
+                "r": 720
+              },
+              "videoGroup": [
+                {
+                  "id": 57106,
+                  "name": "欧美现场",
+                  "alg": "groupTagRank"
+                },
+                {
+                  "id": 59108,
+                  "name": "巡演现场",
+                  "alg": "groupTagRank"
+                },
+                {
+                  "id": 1100,
+                  "name": "音乐现场",
+                  "alg": "groupTagRank"
+                },
+                {
+                  "id": 58100,
+                  "name": "现场",
+                  "alg": "groupTagRank"
+                },
+                {
+                  "id": 5100,
+                  "name": "音乐",
+                  "alg": "groupTagRank"
+                }
+              ],
+              "previewUrl": null,
+              "previewDurationms": 0,
+              "hasRelatedGameAd": false,
+              "markTypes": null,
+              "relateSong": [],
+              "relatedInfo": null,
+              "videoUserLiveInfo": null,
+              "vid": "57B282070780439AB8B7EEBADA7B0BF4",
+              "durationms": 279173,
+              "playTime": 131625,
+              "praisedCount": 1308,
+              "praised": false,
+              "subscribed": false
+            }
+          }
+      ]
+      this.setData({
+        videoList:[...this.data.videoList,...data]
+      })
+    },2000)
   },
 
   /**

8f5d37efa234c077ade07eb17b109936cca10c12
HotJuly <949588380@qq.com>
2020-09-24 10:04:02 +0800

    21.视频模块(视频列表动态效果)

--- pages/video/video.js
+++ pages/video/video.js
@@ -7,17 +7,42 @@ Page({
    */
   data: {
     navList:[],
-    currentId: null
+    currentId: null,
+    videoList:[]
   },
-  changeId(event){
+  async changeId(event){
     // console.log('changeId')
     //标签属性的值一定是字符串类型
     //自定义属性的值的类型,你传给他什么,他就是什么
     // console.log(event.currentTarget.dataset.id)
     // console.log(event.currentTarget.id)
+    
+
+    //同步设置data中的currentId
     this.setData({
-      currentId: event.currentTarget.id*1
+      currentId: event.currentTarget.id*1,
+      videoList:[]
     })
+
+    //显示loading提示框
+    wx.showLoading({
+      title:"正在加载,请稍后..."
+    })
+
+    //等待请求结束,不加await,此处不会等待请求回来
+    await this.getVideoListData();
+    // console.log(2)
+
+    //隐藏loading提示框
+    wx.hideLoading();
+  },
+  async getVideoListData(){
+    let videoListData = await request('/video/group', { id: this.data.currentId })
+    // console.log('videoListData',videoListData)
+    this.setData({
+      videoList: videoListData.datas
+    })
+    // console.log(1)
   },
 
   /**
@@ -30,8 +55,14 @@ Page({
       navList: listData.data.slice(0,14),
       currentId: listData.data[0].id
     })
-    let videoListData = await request('/video/group', { id: 58100})
-    console.log('videoListData',videoListData)
+
+    //发送请求获取videoList数据
+    this.getVideoListData();
+    // let videoListData = await request('/video/group', { id: 58100})
+    // // console.log('videoListData',videoListData)
+    // this.setData({
+    //   videoList: videoListData.datas
+    // })
   },
 
   /**
